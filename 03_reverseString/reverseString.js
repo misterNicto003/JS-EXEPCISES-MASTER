ddc33d6ae7d41e11e010383e5736bf07272e792a
kanuni <mister.nicto006@gmail.com>
2024-04-03 06:09:53 +0300

    revers

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str, num) {
+    let revers = "";
 
-};
+    for(let i = str.length -1;i >=0; i-- ){
+        revers +=str[i];
+
+    }
+    return revers
+}
+ 
+const newStr = reverseString(" ")
+
+
+
 
 // не трогаем код ниже
 module.exports = reverseString;
