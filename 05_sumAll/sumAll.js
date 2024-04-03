5350d47b3afc84e4355249916d7787d1cc594459
kanuni <mister.nicto006@gmail.com>
2024-04-03 17:39:49 +0300

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if( !Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+  if( a < 0 || b < 0  ) return "ERROR"
+  if ( a > b ){
+    const temp = a
+    a = b
+    b = temp
+  }
 
+  let sum = 0;
+    for ( let i = a; i <= b; i++) {
+      sum += i;
+    }
+  
+  
+
+  return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // не трогать
 module.exports = sumAll;
