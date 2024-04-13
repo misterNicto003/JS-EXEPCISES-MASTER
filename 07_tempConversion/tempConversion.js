ea72e29c6b02f9c882cc4aa351666fc8e6cc388e
kanuni <mister.nicto006@gmail.com>
2024-04-13 11:59:05 +0300

    fix

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsia  = (fahrenheit-32)* 5/9
+
+  return +celsia.toFixed(1)
+
+
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsia) {
+  let fahrenheit = celsia * 9/5+32
+  return +fahrenheit.toFixed(1)
 };
 
 // это тоже нельзя (!)
